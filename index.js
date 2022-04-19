c5fedb3b20961efd85a1cdba8cdf291c3c8be19e
Matej Bobek <matejbobek@sudolabs.io>
2022-04-19 14:46:30 +0200

    Change port to env variable

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express')
-const dbConfig = require('../config/db')
+const dbConfig = require('./config/db')
 require('dotenv').config()
 
 const app = express()
 const knex = require('knex')(dbConfig)
+
+const port = process.env.PORT || 3001
 
 app.get('/', async (req, res) => {
   const items = await knex.from('items').select('*')
   res.send({ items, preview_override: process.env.TEST_ENV_VARIABLE })
 })
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000')
-})
+app.listen(port, () => console.log(`Listening on port ${port}`))
