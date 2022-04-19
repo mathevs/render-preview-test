c0976eef153c7438af21a43ff10df0e655eeedd3
Matej Bobek <matejbobek@sudolabs.io>
2022-04-19 15:13:56 +0200

    Handle error in promise

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ const knex = require('knex')(dbConfig)
 const port = process.env.PORT || 3001
 
 app.get('/', async (req, res) => {
-  const items = await knex.from('items').select('*')
-  res.send({ items, preview_override: process.env.TEST_ENV_VARIABLE })
+  try {
+    const items = await knex.from('items').select('*')
+    res.json({ items, preview_override: process.env.TEST_ENV_VARIABLE })
+  } catch(error) {
+    console.error(error)
+  }
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
