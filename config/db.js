d4df6cb442e08fa12d2233b99167c6d1ebdc78c2
Matej Bobek <matejbobek@sudolabs.io>
2022-04-19 13:43:43 +0200

    Fix environment variable usage in db config

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
+
 module.exports = {
   migrations: { directory: '../migrations/' },
   client: 'pg',
-  connection: 'postgres://postgres:postgres@localhost:5434/render_preview_test',
+  connection: process.env.DB_CONNECTION_STRING,
 }
