dae321145217a43fd3bfcc4605c0f817b9ac892b
Matej Bobek <matejbobek@sudolabs.io>
2022-04-21 10:09:05 +0200

    Test preview without database

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const port = process.env.PORT || 3001
 
 app.get('/', async (req, res) => {
   try {
-    const items = await knex.from('items').select('*')
-    res.json({ items, preview_override: process.env.TEST_ENV_VARIABLE })
+    // const items = await knex.from('items').select('*')
+    // res.json({ items, preview_override: process.env.TEST_ENV_VARIABLE })
+    res.json({ preview_override: process.env.TEST_ENV_VARIABLE })
   } catch (error) {
     console.error(error)
     res.json({ error: 'Cannot get data from database' })
